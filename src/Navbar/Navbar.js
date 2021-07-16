aaa414438ec9d831aa3f7e22619c96adfa8f1c4a
Mia Zhu <yz2466@nyu.edu>
2021-07-16 11:11:58 -0700

    Add navbar

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -9,6 +9,8 @@ const NavbarStyled = styled.div`
   position: fixed;
   width: 100%;
   z-index: 999;
+  display: flex;
+  justify-content: space-between;  
 `;
 
 const Logo = styled(Title)`
@@ -17,15 +19,39 @@ const Logo = styled(Title)`
   text-shadow: 1px 1px 4px #380502;
 `;
 
-export function Navbar() {
+const UserStatus = styled.div`
+  color: white; 
+  font-size: 12px; 
+  margin-right: 30px; 
+`
+
+const LoginButton = styled.span`
+  cursor: pointer; 
+`
+
+export function Navbar({login, loggedIn, logout}) {
   return (
     <NavbarStyled>
       <Logo>
         Ding Tai Fung{" "}
-        <span role="img" aria-label="pizza slice">
-        🥟
+        <span role="img" aria-label="dtf">
+        🥟 
         </span>
       </Logo>
+      <UserStatus>
+        {loggedIn !== "loading" ? (
+          <>
+           
+            {loggedIn ? (
+              <LoginButton onClick={logout}> Log out </LoginButton>
+            ) : (
+              <LoginButton onClick={login}> Log in / Sign up </LoginButton>
+            )}
+          </>
+        ) : (
+          "loading..."
+        )}
+      </UserStatus>
     </NavbarStyled>
   );
 }
